afe554f0ca932f91b880b30496d022d6a5550011
Lucas Robert <neo_age50@hotmail.com>
2020-06-15 16:09:08 -0400

    little fixes to the checklist and notepage to display better

--- WHMIS/Views/PocketGuide/_Checklist.xaml.cs
+++ WHMIS/Views/PocketGuide/_Checklist.xaml.cs
@@ -56,7 +56,8 @@ namespace WHMIS.Views.PocketGuide
             Entry entry = sender as Entry;
 
             ChecklistItem item = (ChecklistItem)entry.BindingContext;
-
+            item.Notes = entry.Text;
+            Console.WriteLine("item.Notes" + item.Notes);
             checklistVM.UpdateNotes(item);
         }
 
@@ -71,13 +72,13 @@ namespace WHMIS.Views.PocketGuide
 
                 ChecklistItem data = (ChecklistItem)senderBox.BindingContext;
 
+                data.IsChecked = e.Value;
                 if (!senderBox.IsChecked)
                 {
                     //box has been unchecked, deleted entry
                     data.Date = new DateTime(0001, 01, 01);
                     data.Notes = "";
                 }
-                data.IsChecked = e.Value;
 
                 checklistVM.UpdateNotes(data);
 
